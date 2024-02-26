3c1fec15284433529f8004a2d9572b36af1629fe
Cheryl Lopez <clopez51@stu.southtexascollege.edu>
2024-02-25 22:27:39 -0600

    todo list is partially working

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+import {ref} from 'vue'
+const todo = ref([])
+const name = ref('')
+const input_content = ref('')
+
+const addTodo = () => {
+    if(input_content.value.trim() ===){
+        return
+    }
+}
+
+todo.value.push({
+    content: input_content.value,
+})
+
+input_content.value = ''
+//console.log(todos)
+
+</script>
+
+<template>
+    <main class="app">
+        <section class="greeting">
+            <h2 class="title">
+                What's up, <inpute type="text" placeholder="name" v-model="name" />
+                <!--{{name}}--> 
+            </h2>
+        </section>
+
+        <section class="create-todo">
+            <h3>CREATE A TODO</h3>
+            <form @submit.prevent="addTodo">
+                <h4>Whats on your todo list?</h4>
+                <input type="text" placeholder="e.g, Make a video" v-model="input_content" />
+                {{ input_content }}
+            </form>
+        </section>
+
+        <section class="todo-list">
+
+        </section>
+    </main>
+</template>
